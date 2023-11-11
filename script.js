bd84dd565fa3328b98c84ad476a5768ba170e3d9
camarqu4 <149449175+camarqu4@users.noreply.github.com>
2023-11-11 16:04:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -17,10 +17,33 @@ function validateForm() {
   var email = document.getElementById('email').value;
   var phone = document.getElementById('phone').value;
   var pickupTime = document.getElementById('pickupTime').value;
+  // Add validation logic here if needed
+}
+
+// Function to load cart from localStorage and populate the order table
+function loadCartAndPopulateOrderTable() {
+  var cart = JSON.parse(localStorage.getItem('cart') || '{}');
+  var orderTable = document.getElementById('orderTable');
+
+  Object.keys(cart).forEach(function(productName) {
+    var product = cart[productName];
+    var row = orderTable.insertRow();
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+
+    cell1.textContent = productName;
+    cell2.innerHTML = `<input type="number" min="0" value="${product.quantity}" class="quantity-input" data-price="${product.price}" onchange="updatePrice(this)">`;
+    cell3.classList.add('product-price');
+    cell3.textContent = '$' + (product.price * product.quantity).toFixed(2);
+  });
 }
 
 // Event listener for DOM content loaded
 document.addEventListener('DOMContentLoaded', function() {
+  // Load cart and populate order table
+  loadCartAndPopulateOrderTable();
+
   // Attach event listeners to the quantity inputs
   var quantityInputs = document.querySelectorAll('.quantity-input');
   quantityInputs.forEach(function(input) {
