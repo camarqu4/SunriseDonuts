cfc3f7f53a66a1897a2febc253821bf300d1ed3e
camarqu4 <149449175+camarqu4@users.noreply.github.com>
2023-11-11 21:43:17 -0700

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,5 +1,24 @@
-// cart.js
-
 function addToCart(productName, price, quantityId) {
-    // ... (the addToCart function code)
+    const quantity = parseInt(document.getElementById(quantityId).value);
+
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let productIndex = cart.findIndex(item => item.name === productName);
+
+    if (productIndex !== -1) {
+        // Update quantity if product is already in cart
+        cart[productIndex].quantity += quantity;
+    } else {
+        // Add new product to cart
+        const product = {
+            name: productName,
+            price: price,
+            quantity: quantity,
+            // Assuming you add description here
+            description: 'Product description here' // Update this accordingly
+        };
+        cart.push(product);
+    }
+
+    // Save the cart to Local Storage
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
