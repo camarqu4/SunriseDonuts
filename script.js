3c9449abc699257aae9e0130ad0b60bb4ae0f651
camarqu4 <149449175+camarqu4@users.noreply.github.com>
2023-11-28 00:14:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ function updatePrice(input) {
   var quantity = input.valueAsNumber; 
   var unitPrice = parseFloat(input.dataset.price); 
   var priceCell = input.closest('tr').querySelector('.product-price'); 
-
-
   var newPrice = unitPrice * quantity;
   
   priceCell.textContent = '$' + newPrice.toFixed(2);
+
+  updateTotalCost(); // Update the total cost whenever a price is updated
 }
 
+function updateTotalCost() {
+  var total = 0;
+  var quantityInputs = document.querySelectorAll('.quantity-input');
+  quantityInputs.forEach(function(input) {
+      var price = parseFloat(input.getAttribute('data-price'));
+      var quantity = parseInt(input.value);
+      total += price * quantity;
+  });
+  document.getElementById('totalCost').textContent = '$' + total.toFixed(2);
+}
 
 function validateForm() {
   var firstName = document.getElementById('firstName').value;
@@ -16,28 +26,24 @@ function validateForm() {
   var email = document.getElementById('email').value;
   var phone = document.getElementById('phone').value;
   var pickupTime = document.getElementById('pickupTime').value;
+
+  // Basic validation example
+  if (!firstName || !lastName || !email || !phone || !pickupTime) {
+    alert('Please fill out all fields.');
+    return false;
+  }
+  return true;
 }
 
-
 document.addEventListener('DOMContentLoaded', function() {
-  
   var quantityInputs = document.querySelectorAll('.quantity-input');
   quantityInputs.forEach(function(input) {
     input.addEventListener('change', function() {
       updatePrice(this);
     });
+    updatePrice(input); // Update the price initially for each input
   });
 
-  function updateTotalCost() {
-        var total = 0;
-        quantityInputs.forEach(function(input) {
-            var price = parseFloat(input.getAttribute('data-price'));
-            var quantity = parseInt(input.value);
-            total += price * quantity;
-        });
-        document.getElementById('totalCost').textContent = '$' + total.toFixed(2);
-    }
-  
   var orderForm = document.getElementById('orderForm');
   orderForm.addEventListener('submit', function(event) {
     if (!validateForm()) {
